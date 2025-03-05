28c20166459769dd272b195eea0dc21c576f5691
chento chea <cheachento007@gmail.com>
2025-03-05 22:15:13 +0700

    few

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,6 @@
 import { API_URL } from "@/constant/constant";
 
 export const fetchFromApi = async (apiFunction, bodyData = {}) => {
-<<<<<<< HEAD
   const myHeaders = new Headers();
   myHeaders.append("Cookie", "PHPSESSID=3u01lnipofpe8o5f83sbu8mm41");
 
@@ -19,40 +18,6 @@ export const fetchFromApi = async (apiFunction, bodyData = {}) => {
     headers: myHeaders,
     body: formData,
     redirect: "follow",
-=======
-    const myHeaders = new Headers();
-    myHeaders.append("Cookie", "PHPSESSID=3u01lnipofpe8o5f83sbu8mm41");
-  
-    // Create FormData and append api_function
-    const formdata = new FormData();
-    formdata.append("api_function", apiFunction);
-  
-    // Append extra body data if provided
-    for (const [key, value] of Object.entries(bodyData)) {
-      formdata.append(key, value);
-    }
-  
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: formdata,
-      redirect: "follow",
-    };
-  
-    try {
-      const response = await fetch(
-        "https://www.giantibis.com/beta1/api-booking.php",
-        requestOptions
-      );
-      const result = await response.text();
-      // console.log(result);
-      // return result;
-      return JSON.parse(result);
-    } catch (error) {
-      console.error("API Error:", error);
-      throw error;
-    }
->>>>>>> fd5007466c9038df5c78265be3f48dae4ab50c8f
   };
 
   try {
