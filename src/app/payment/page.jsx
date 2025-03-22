d627166615f0da3141aaf8fca50b98e7ecfb999b
chento chea <cheachento007@gmail.com>
2025-03-22 22:08:18 +0700

    dwq

--- src/app/payment/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import PopupPayment from '@/components/features/payments/PopupPayment'
-import React from 'react'
-
-export default function page() {
-  return (
-    <PopupPayment routeSelected={null} selectedSeat={ null} />
-  )
-}
