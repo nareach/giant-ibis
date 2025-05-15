27b01d6be449a04dfe9386eb2dc9b54647d2e598
chento007 <chentochea007@gmail.com>
2025-05-15 22:08:11 +0700

    fixed

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,15 +1,14 @@
 import { NextResponse } from 'next/server';
-import fs from 'fs';
 import puppeteer from 'puppeteer';
 
 export async function GET() {
   try {
-    const html = fs.readFileSync('template.html', 'utf8');
+    
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.setContent(html, { waitUntil: 'networkidle0' });
+    await page.setContent(`<h1>Hello chento</h1>`, { waitUntil: 'networkidle0' });
 
     const pdfBuffer = await page.pdf({
       format: 'A4',
